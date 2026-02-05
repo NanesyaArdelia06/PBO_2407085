fb7539b24304405a43b8fd9ba3f3d4a3c59d25fa
NanesyaArdelia06 <nanesyaardelia@gmail.com>
2026-02-05 09:45:27 +0700

    praktikum 1 2407085

---
+++ latihan 1/src/latihan_pbo/latihan1_2407085/Main.java
@@ -0,0 +1,8 @@
+package latihan_pbo.latihan1_2407085;
+
+public class Main {
+    Run | Debug
+    public static void main(String[] args) {
+        System.out.println("Praktikum 1 2407085"); 
+    }
+}
